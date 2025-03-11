fec8229f84fc750b1054414c5481636c05037100
MoeDev <143090643+MoeDevelops@users.noreply.github.com>
2025-03-11 21:56:23 +0100

    Change from ffi to gleam_time, support js

--- src/youid_ffi.erl
+++ src/youid_ffi.erl
@@ -7,14 +7,14 @@ mac_address() ->
         _ -> {error, nil}
     end.
 
-mac_address(Ifs) -> 
+mac_address(Ifs) ->
     case Ifs of
         [] -> {error, nil};
         [{_Name, Props} | Rest] ->
             case proplists:get_value(hwaddr, Props) of
                 undefined -> mac_address(Rest);
                 [] -> mac_address(Rest);
-                Ints -> 
+                Ints ->
                     case lists:all(fun(X) -> X =/= 0 end, Ints) of
                         true -> erlang:list_to_binary(Ints);
                         false -> mac_address(Rest)
